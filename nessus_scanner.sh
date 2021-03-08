255ae66745f84814fe5d092ba7ddd6b1857c85a6
Yury Sagalaev <yury.sagalaev@acronis.com>
2021-03-09 00:58:42 +0300

    update registry

--- nessus_scanner.sh
+++ nessus_scanner.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-docker image rm yrok472/nessus_scanner
+# "cd" into "nessus_scanner" before script running
+
+# Preconditions:
+# docker login https://harbor.smartvz.site # enter creds from https://harbor.smartvz.site
+
+docker image rm infra/nessus_scanner
 docker image rm nessus_scanner
 docker build . -t nessus_scanner:latest
-docker tag nessus_scanner yrok472/nessus_scanner:latest
-docker push yrok472/nessus_scanner
+docker tag nessus_scanner harbor.smartvz.site/infra/nessus_scanner:latest
+docker push harbor.smartvz.site/infra/nessus_scanner:latest
